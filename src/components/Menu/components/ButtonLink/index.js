ef8893ecabfcb9199e8d7f5843eff33aedd549bf
jorgevilasboas <jorge.sergipe@gmail.com>
2020-08-07 18:34:21 -0300

    Imersao react - Jorgeflix

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function ButtonLink(props) {
+    console.log(props)
+    return (
+        <a href={props.href} className={props.className}>
+            {props.children}
+
+        </a>
+    )
+}
+
+export default ButtonLink;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+import ButtonLink from './components/ButtonLink';
+
+// import { Container } from './styles';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="JorgeFlix Logo" />
+            </a>
+            <ButtonLink className="ButtonLink" href="">
+                Novo Vídeo
+            </ButtonLink>
+        </nav>
+    )
+}
+
+export default Menu;
